bc993b5cf1e7c5ab31449a2f7a58579b5f728236
pyopywhiz <pyopywhiz@gmail.com>
2023-08-26 22:02:21 +0700

    Reformat

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-from fastapi import FastAPI
+from app.main import app
 
-app = FastAPI()
+if __name__ == "__main__":
+    import uvicorn
 
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
+    uvicorn.run(app, host="0.0.0.0", port=8000)
