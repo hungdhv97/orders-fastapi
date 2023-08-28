526b2cdff3af1c34cf19be5cf3cc1ff274f7d19b
Hung.Thai (Patrick) /Sotatek/DnU <e.hung.thai@wesang.com>
2023-08-28 17:15:36 +0700

    Update user router

--- app/api/v1/user_router.py
+++ app/api/v1/user_router.py
@@ -20,29 +20,29 @@ async def create_user(user: CreateUserRequest) -> UserResponse:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Username already exists")
 
 
-@router.get("/user/{username}", response_model=UserResponse)
-async def get_user(username: str) -> UserResponse:
-    user = User.objects(username=username).first()
+@router.get("/user/{user_id}", response_model=UserResponse)
+async def get_user(user_id: str) -> UserResponse:
+    user = User.objects(id=user_id).first()
     if user:
         return UserResponse(id=str(user.id), **user.to_mongo())
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
 
-@router.put("/user/{username}", response_model=UserResponse)
-async def update_user(username: str, user_update: UpdateUserRequest) -> UserResponse:
-    user = User.objects(username=username).first()
+@router.put("/user/{user_id}", response_model=UserResponse)
+async def update_user(user_id: str, user_update: UpdateUserRequest) -> UserResponse:
+    user = User.objects(id=user_id).first()
     if user:
         for attr, value in user_update.model_dump().items():
             if value is not None:
                 setattr(user, attr, value)
         user.save()
-        return UserResponse(**user.to_mongo())
+        return UserResponse(id=str(user.id), **user.to_mongo())
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
 
 
-@router.delete("/user/{username}", response_model=dict)
-async def delete_user(username: str):
-    user = User.objects(username=username).first()
+@router.delete("/user/{user_id}", response_model=dict)
+async def delete_user(user_id: str):
+    user = User.objects(id=user_id).first()
     if user:
         user.delete()
         return {"message": "User deleted"}
