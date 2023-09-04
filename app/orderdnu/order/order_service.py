4b3fb3e87e1f09901544c56ebfc9d279f29d9216
pyopywhiz <pyopywhiz@gmail.com>
2023-09-04 14:19:05 +0700

    Update Order router

--- app/orderdnu/order/order_service.py
+++ app/orderdnu/order/order_service.py
@@ -1,5 +1,7 @@
+from typing import List, Optional
+
 from fastapi import HTTPException
-from mongoengine import NotUniqueError
+from mongoengine import NotUniqueError, DoesNotExist, ValidationError
 from starlette import status
 
 from app.orderdnu.order.order_document import OrderDocument
@@ -16,18 +18,39 @@ class OrderService:
             user = await user_service.get_user(user_id)
             new_order = OrderDocument(user=UserDocument(**dict(user)), food_name=food_name, price=price)
             new_order.save()
-            return Order.model_validate({"id": str(new_order.id), "user_id": str(new_order.user.id), **new_order.to_mongo()})
+            return Order.model_validate(
+                {"id": str(new_order.id), "user_id": str(new_order.user.id), **new_order.to_mongo()})
         except NotUniqueError:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Order already exists")
 
-    async def get_order(self) -> Order:
-        pass
+    async def get_order(self, order_id: str) -> Order:
+        try:
+            order = UserDocument.objects.get(id=order_id)
+            return Order.model_validate({"id": str(order.id), "user_id": str(order.user.id), **order.to_mongo()})
+        except ValidationError as e:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+        except DoesNotExist:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found")
 
-    async def get_orders(self) -> Order:
-        pass
+    async def get_orders(self, skip: int = 0, limit: int = 10) -> List[Order]:
+        orders = OrderDocument.objects.skip(skip).limit(limit)
+        return [Order.model_validate({"id": str(order.id), "user_id": str(order.user.id), **order.to_mongo()}) for order
+                in orders]
 
-    async def update_order(self) -> Order:
-        pass
+    async def update_order(self, order_id: str, food_name: Optional[str], price: Optional[int]) -> Order:
+        order = await self.get_order(order_id)
+        user = await user_service.get_user(order.user_id)
+        if food_name:
+            order.food_name = food_name
+        if price:
+            order.price = price
+        UserDocument(user=UserDocument(**dict(user)), **dict(order)).save()
+        return order
 
-    async def delete_order(self) -> Order:
-        pass
+    async def delete_order(self, order_id: str) -> None:
+        try:
+            OrderDocument.objects.get(id=order_id).delete()
+        except ValidationError as e:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
+        except DoesNotExist:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found")
--- app/orderdnu/order/v1/order_router.py
+++ app/orderdnu/order/v1/order_router.py
@@ -1,52 +1,39 @@
 from typing import List
 
-from fastapi import APIRouter, HTTPException
-from mongoengine import NotUniqueError, DoesNotExist, ValidationError
-from starlette import status
+from fastapi import APIRouter
 
-from app.orderdnu.order.order_document import OrderDocument
 from app.orderdnu.order.order_model import OrderResponse, CreateOrderRequest, UpdateOrderRequest
 from app.orderdnu.order.order_service import OrderService
-from app.orderdnu.user.user_document import UserDocument
 
 router = APIRouter()
 order_service = OrderService()
 
+
 @router.post("/", response_model=OrderResponse)
 async def create_order(order: CreateOrderRequest) -> OrderResponse:
-    new_order = await order_service.create_order(order.user_id, order.food_name, order.price )
+    new_order = await order_service.create_order(order.user_id, order.food_name, order.price)
     return OrderResponse.model_validate(new_order.model_dump())
+
 
 @router.get("/{order_id}", response_model=OrderResponse)
 async def get_order(order_id: str) -> OrderResponse:
-    order = OrderDocument.objects(id=order_id).first()
-    if order:
-        return OrderResponse(id=str(order.id), **order.to_mongo())
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found")
+    order = await order_service.get_order(order_id)
+    return OrderResponse.model_validate(order.model_dump())
 
 
 @router.put("/{order_id}", response_model=OrderResponse)
 async def update_order(order_id: str, order_update: UpdateOrderRequest) -> OrderResponse:
-    order = OrderDocument.objects(id=order_id).first()
-    if order:
-        for attr, value in order_update.model_dump().items():
-            if value is not None:
-                setattr(order, attr, value)
-        order.save()
-        return OrderResponse(**order.to_mongo())
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found")
+    order = await order_service.update_order(order_id, order_update.food_name, order_update.price)
+    return OrderResponse.model_validate(order.model_dump())
 
 
 @router.delete("/{order_id}", response_model=dict)
-async def delete_order(order_id: str):
-    order = OrderDocument.objects(id=order_id).first()
-    if order:
-        order.delete()
-        return {"message": "Order deleted"}
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Order not found")
+async def delete_order(order_id: str) -> dict:
+    await order_service.delete_order(order_id)
+    return {"message": "Order deleted"}
 
 
 @router.get("/", response_model=List[OrderResponse])
 async def get_orders(skip: int = 0, limit: int = 10) -> List[OrderResponse]:
-    orders = OrderDocument.objects.skip(skip).limit(limit)
-    return [OrderResponse(id=str(order.id), user_id=str(order.user.id), **order.to_mongo()) for order in orders]
+    orders = await order_service.get_orders(skip, limit)
+    return [OrderResponse.model_validate(order.model_dump()) for order in orders]
