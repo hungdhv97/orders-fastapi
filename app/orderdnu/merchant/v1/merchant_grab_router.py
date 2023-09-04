f2ee9041ebb990e9b7573b6c1320070dec2e6a3c
pyopywhiz <pyopywhiz@gmail.com>
2023-09-04 12:13:13 +0700

    Update example

--- app/orderdnu/merchant/v1/merchant_grab_router.py
+++ app/orderdnu/merchant/v1/merchant_grab_router.py
@@ -1,4 +1,6 @@
-from fastapi import APIRouter
+from typing import Annotated
+
+from fastapi import APIRouter, Path
 
 from app.clients.grab.grab_client import GrabClient
 from app.orderdnu.merchant.merchant_model import MerchantResponse
@@ -11,7 +13,6 @@ menu_service = MenuService(grab_client)
 
 
 @router.get("/grab/merchants/{merchant_id}", response_model=MerchantResponse)
-async def get_menus_grab(merchant_id: str) -> MerchantResponse:
-    """5-C3C2T8MUVN4HLT"""
+async def get_menus_grab(merchant_id: Annotated[str, Path(..., example="5-C3C2T8MUVN4HLT")]) -> MerchantResponse:
     menu_data = await menu_service.get_menus_grab(merchant_id)
     return menu_data
