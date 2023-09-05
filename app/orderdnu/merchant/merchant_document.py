99c9f4592c25e52da9d55b10b1b838f5e6b245be
pyopywhiz <pyopywhiz@gmail.com>
2023-09-05 15:16:38 +0700

    Update Create Merchant

--- app/orderdnu/merchant/merchant_document.py
+++ app/orderdnu/merchant/merchant_document.py
@@ -6,5 +6,5 @@ from app.orderdnu.user.user_document import UserDocument
 
 class MerchantDocument(Document):
     merchant_id = StringField(required=True, unique_with='delivery_type')
-    delivery_type = EnumField(DeliveryEnum, default=DeliveryEnum.GRAB, required=True)
+    delivery_type = EnumField(DeliveryEnum, default=DeliveryEnum.GRAB, required=True, unique_with='user')
     user = ReferenceField(UserDocument)
